d24b97ff9f53248ae93f3c7245b04cd19450acf8
name <ankhanh1566@gmail.com>
2026-04-06 16:38:32 +0700

    first co,mmit

---
+++ electron-main.js
@@ -0,0 +1,67 @@
+const { app, BrowserWindow, globalShortcut, screen } = require('electron');
+const path = require('path');
+
+let overlayWindow;
+
+function createOverlayWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  overlayWindow = new BrowserWindow({
+    width,
+    height,
+    x: 0,
+    y: 0,
+    frame: false,
+    transparent: true,
+    opacity: 0.65,
+    hasShadow: false,
+    resizable: false,
+    movable: true,
+    minimizable: false,
+    maximizable: false,
+    fullscreen: true,
+    fullscreenable: true,
+    focusable: false,
+    skipTaskbar: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+
+  overlayWindow.setAlwaysOnTop(true, 'screen-saver');
+  overlayWindow.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
+  overlayWindow.setIgnoreMouseEvents(true, { forward: true });
+  overlayWindow.loadFile(path.join(__dirname, 'run-triangles.html'));
+
+  overlayWindow.on('closed', () => {
+    overlayWindow = null;
+  });
+}
+
+app.whenReady().then(() => {
+  createOverlayWindow();
+
+  // Quick exits for overlay mode.
+  globalShortcut.register('Escape', () => {
+    app.quit();
+  });
+  globalShortcut.register('CommandOrControl+Shift+Q', () => {
+    app.quit();
+  });
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createOverlayWindow();
+  }
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
